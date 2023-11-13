ac8e8f5c15ad4b89f0a7c1fd339cf7f8a0641d42
GONZALEZ SORO PEDRO <47572859@ort>
2023-11-13 12:00:07 -0300

    solcion de que anda una vez si y otra no
    sw

--- airswipe.py
+++ airswipe.py
@@ -19,7 +19,7 @@ def receive_data():
     global datos
     try:
         data = request.get_json(force=True)  # Indica que se espera un JSON
-        print(data)
+        flattened_datos = []
 
         vx = int(data[0]) * 2.8
         vy = int(data[1]) * 2.2
@@ -35,30 +35,38 @@ def receive_data():
             del datos[0]
             del datos[1]
 
-        flattened_datos = [item for sublist in datos for item in sublist]
-
         def gestos_normales():
+            for _ in range(10):
+                if len(datos) <= 11:
+                    datos.append(gyro)
+
+                if len(datos) > 11:
+                    del datos[0]
+                    del datos[1]
+
+                flattened_datos = [item for sublist in datos for item in sublist]
+
             x_padded = np.array([flattened_datos])
             predictions = model.predict(x_padded)
             
             # Obtener la probabilidad máxima de predicción
-            max_probability = np.max(predictions)
+            #max_probability = np.max(predictions)
             predicted_label = label_encoder.inverse_transform(np.argmax(predictions, axis=1))[0]
             
-            if max_probability >= 0.9:
+            #if max_probability >= 0.9:
             # Imprimir la predicción
-                print(f"Movimiento detectado: {predicted_label}")
+            print(f"Movimiento detectado: {predicted_label}")
                             
-                if predicted_label == "R":
-                    pa.hotkey('alt', 'tab')
-                if predicted_label == "D":
-                    pa.press('volumedown')
-                if predicted_label == "U":
-                    pa.press('volumeup')
-                if predicted_label == "L":
-                    pa.press('volumemute')
-                if predicted_label == "F":
-                    print("Nada")
+            if predicted_label == "R":
+                pa.hotkey('alt', 'tab')
+            if predicted_label == "D":
+                pa.press('volumedown')
+            if predicted_label == "U":
+                pa.press('volumeup')
+            if predicted_label == "L":
+                pa.press('volumemute')
+            if predicted_label == "F":
+                print("Nada")
 
         def gestos_presentacion():
             x_padded = np.array([flattened_datos])
@@ -87,8 +95,8 @@ def receive_data():
         if (alternar == 1):
             gestos_normales()
 
-        if(presentacion == 1):
-            gestos_presentacion()
+        #if(presentacion == 1):
+        #    gestos_presentacion()
 
         
         # Mover el mouse suavizado
